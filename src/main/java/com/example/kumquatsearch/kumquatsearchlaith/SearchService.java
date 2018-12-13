37c7b041689367eb3ca022af9e1c94024e755a94
RashmiMuraharishetti <raymondhedgecock@gmail.com>
2018-12-13 14:46:53 -0700

    Added filters Brand,Color,Price,Multiple

--- src/main/java/com/example/kumquatsearch/kumquatsearchlaith/SearchService.java
+++ src/main/java/com/example/kumquatsearch/kumquatsearchlaith/SearchService.java
@@ -1,4 +1,43 @@
 package com.example.kumquatsearch.kumquatsearchlaith;
 
+import com.example.kumquatsearch.kumquatsearchlaith.model.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class SearchService {
+
+    private List<Product> products= new ArrayList<Product>();
+
+
+    public Iterable<Product> searchByBrand(){
+
+
+
+        return this.products.stream().filter(p -> p.getBrand().equalsIgnoreCase("Brand")).collect(Collectors.toList());
+    }
+
+    public Iterable<Product> searchByColor(){
+
+
+
+        return this.products.stream().filter(p -> p.getColor().equalsIgnoreCase("Color")).collect(Collectors.toList());
+    }
+
+    public Iterable<Product> searchByPriceRange(){
+
+
+
+        return this.products.stream().filter(p -> p.getPrice()>=13).filter(p->p.getPrice()<=6).collect(Collectors.toList());
+    }
+
+    public Iterable<Product> searchByMultiple(){
+
+
+
+        return this.products.stream().filter(p -> p.getColor().equalsIgnoreCase("Color"))
+                .filter(p->p.getBrand().equalsIgnoreCase("Brand")).collect(Collectors.toList());
+    }
+
 }
